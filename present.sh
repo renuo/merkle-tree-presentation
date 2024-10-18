d5482118eccbdff12ef3ed64bdc08f3b4c610f61
Gianlorenzo Occhipinti <gianlorenzo.occhipinti@renuo.ch>
2024-10-18 14:10:29 +0200

    Update present.sh

--- present.sh
+++ present.sh
@@ -1,5 +1,2 @@
 #!/bin/zsh
-
-
-manim-slides render presentation.py MerkleTreePresentation
-manim-slides convert MerkleTreePresentation presentation.html --open
+manim-slides render presentation.py MerkleTreePresentation && manim-slides convert MerkleTreePresentation presentation.html --open
